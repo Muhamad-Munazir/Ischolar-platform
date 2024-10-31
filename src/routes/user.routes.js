e7d31eb7a933de882d267958cf1ee6730fd345df
Muhammad Munazir <munazirx7@gmail.com>
2024-11-01 00:33:37 +0530

    add RefreshToken endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, RefreshAccessToken, registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
@@ -10,6 +10,7 @@ router.route("/login").post(loginUser)
 
 //Secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/refres-token").post(RefreshAccessToken)
 
 
 
